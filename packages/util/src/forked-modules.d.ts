557ebf5dd39b0e0fe3569f88a5215fefa3be4120
Сковорода Никита Андреевич <chalkerx@gmail.com>
2021-08-10 06:43:52 +0300

    util: lint

--- packages/util/src/forked-modules.d.ts
+++ packages/util/src/forked-modules.d.ts
@@ -1,11 +1,11 @@
 declare module '@exodus/secp256k1' {
-  export * from 'secp256k1';
-  import secp256k1 from 'secp256k1';
-  export default secp256k1;
+  export * from 'secp256k1'
+  import secp256k1 from 'secp256k1'
+  export default secp256k1
 }
 
 declare module '@exodus/keccak' {
-  export * from 'keccak';
-  import keccak from 'keccak';
-  export default keccak;
+  export * from 'keccak'
+  import keccak from 'keccak'
+  export default keccak
 }
--- packages/util/src/keccak.ts
+++ packages/util/src/keccak.ts
@@ -1,17 +1,17 @@
 // Compatible with https://www.npmjs.com/package/ethereum-cryptography
 
 /// <reference path="forked-modules.d.ts"/>
-import { default as createKeccakHash, KeccakAlgorithm } from '@exodus/keccak';
+import { default as createKeccakHash, KeccakAlgorithm } from '@exodus/keccak'
 
 function createHashFunction(algorithm: KeccakAlgorithm): (msg: Buffer) => Buffer {
   return msg => {
-    const hash = createKeccakHash(algorithm);
-    hash.update(msg);
-    return Buffer.from(hash.digest());
-  };
+    const hash = createKeccakHash(algorithm)
+    hash.update(msg)
+    return Buffer.from(hash.digest())
+  }
 }
 
-export const keccak224 = createHashFunction('keccak224');
-export const keccak256 = createHashFunction('keccak256');
-export const keccak384 = createHashFunction('keccak384');
-export const keccak512 = createHashFunction('keccak512');
+export const keccak224 = createHashFunction('keccak224')
+export const keccak256 = createHashFunction('keccak256')
+export const keccak384 = createHashFunction('keccak384')
+export const keccak512 = createHashFunction('keccak512')
